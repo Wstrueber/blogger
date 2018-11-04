9c6f8a50c67f2bbb571f8c154af67c2839bb53c3
WIlliam Strueber <william.strueber@gmail.com>
2018-11-04 13:34:12 +0100

    Updates

---
+++ precache-manifest.05be9ba9ad6914abab302b6c103f9e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a654a8816f776d6b96e",
+    "url": "/blogger/static/js/runtime~main.2a654a88.js"
+  },
+  {
+    "revision": "b8c6cc08381fb275616c",
+    "url": "/blogger/static/js/main.b8c6cc08.chunk.js"
+  },
+  {
+    "revision": "b08ae09cc23d962890d8",
+    "url": "/blogger/static/js/1.b08ae09c.chunk.js"
+  },
+  {
+    "revision": "b8c6cc08381fb275616c",
+    "url": "/blogger/static/css/main.54fd1c68.chunk.css"
+  },
+  {
+    "revision": "93a156b4857e3dfc5c65e2caeb57a651",
+    "url": "/blogger/index.html"
+  }
+];
